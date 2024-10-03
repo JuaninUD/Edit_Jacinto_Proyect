0fcec464fd562e0e55015762342148a252b26d4c
JuaninUD <juaaramirezp@udistrital.edu.co>
2024-10-03 10:14:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,63 +27,135 @@ char men[40];
 
 char comando[3]= {0x01, 0x60,0xA0};
 char data[2];
-
-
 const char *mensaje_inicio = "Arranque del programa\n\r";
+// Funciones 
+
+void tiempo(void);
+void message(void);
+void screen(void);
+void start(void);
+void formula(void);
+void clean(void);
+void hello(void);
+void watch(void);
+void voltage(void);
+void temperatura(void);
+void form(void);
+void time_temp(void);
+void registro(void);
+
+
+
+void time(void){
+    ThisThread::sleep_for(3000ms);
+}
+
+void time_temp(void)
+{
+    ThisThread::sleep_for(2000ms);
+}
 
 int main()
 { 
    // arranque del programa      
-    oled.begin();
-    oled.setTextSize(1);
-    oled.setTextColor(1);
-    oled.display();
-    ThisThread::sleep_for(3000ms);
-    // Limpiar Pantalla
-    oled.clearDisplay();
-    oled.display();
-    oled.printf( "Test\r\n" );
-    oled.display();
+    message(); //Texto Inicio
+    start(); // Edita la pantalla
+    time();
+    screen(); // Inicia Pantalla
+    
+
     //arranque del sensor de temperatura
      i2c.write(TMP102_ADDRESS, comando, 3);
-    // saludo por el puerto serial
-    serial.write(mensaje_inicio, strlen(mensaje_inicio));
+    // Saludo
+    hello();
 
     while (true) {
         // Lectura del sensor analogo (Potenciometro)
-        Vin = ain*3.3;
-        ent = int(Vin); 
-        dec = int((Vin-ent)*10000); 
+        formula();
         // Visualizacion
-        oled.clearDisplay();
-        oled.display();
-        // formateo de la cadena de caracteres
-        sprintf(men, "El voltaje es:\n\r %01u.%04u volts \n\r", ent, dec);
-        oled.setTextCursor(0,2);
-        oled.printf(men);
-        oled.display();
+        clean();
+        // Muestra Voltage Pantalla Oled
+        voltage();
+
         // impresion puerto serie
         serial.write(men, strlen(men));
         //Lectura sensor I2C
         // Leer el registro de temperatura
-        comando[0] = 0; // Registro de temperatura
-        i2c.write(TMP102_ADDRESS, comando, 1); // Enviar el comando para leer
-        i2c.read(TMP102_ADDRESS, data, 2); // Leer 2 bytes
-        int16_t temp = (data[0] << 4) | (data[1] >> 4);
-        float Temperatura = temp *0.0625; 
-        ent = int(Temperatura); 
-        dec = int((Temperatura-ent)*10000); 
+        registro();
+        form();
 
-        ThisThread::sleep_for(2000ms);
-        oled.clearDisplay();
-        oled.display();
-        sprintf(men, "La Temperatura es:\n\r %01u.%04u Celsius\n\r", ent, dec);
-        oled.setTextCursor(0,2);
-        oled.printf(men);
-        oled.display();
+        time_temp();
+
+        clean();
+
+        temperatura();
         // impresion puerto serie
         serial.write(men, strlen(men));        
 
         ThisThread::sleep_for(tiempo_muestreo);
     }
+}
+
+void hello(void)
+{
+    // saludo por el puerto serial
+    serial.write(mensaje_inicio, strlen(mensaje_inicio));
+}
+
+void start(void)
+{
+    oled.begin();
+    oled.setTextSize(1);
+    oled.setTextColor(1);
+    oled.display();
+}
+
+void clean(void)
+{
+    oled.clearDisplay();
+    oled.display();
+}
+
+void formula(void)
+{
+    Vin = ain*3.3;
+    ent = int(Vin); 
+    dec = int((Vin-ent)*10000); 
+}
+
+void watch(void)
+{
+    oled.clearDisplay();
+    oled.display();
+}
+
+void voltage(void)
+{
+    sprintf(men, "El voltaje es:\n\r %01u.%04u volts \n\r", ent, dec);
+    oled.setTextCursor(0,2);
+    oled.printf(men);
+    oled.display();
+}
+
+void registro(void)
+{
+    comando[0] = 0; // Registro de temperatura
+    i2c.write(TMP102_ADDRESS, comando, 1); // Enviar el comando para leer
+    i2c.read(TMP102_ADDRESS, data, 2); // Leer 2 bytes
+}
+
+void form(void)
+{
+    int16_t temp = (data[0] << 4) | (data[1] >> 4);
+    float Temperatura = temp *0.0625; 
+    ent = int(Temperatura); 
+    dec = int((Temperatura-ent)*10000); 
+}
+
+void temperatura()
+{
+    sprintf(men, "La Temperatura es:\n\r %01u.%04u Celsius\n\r", ent, dec);
+    oled.setTextCursor(0,2);
+    oled.printf(men);
+    oled.display();
 }
